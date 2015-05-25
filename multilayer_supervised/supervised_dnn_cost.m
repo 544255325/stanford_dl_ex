e0553acc817ef86f9eaa4b1ef60a44178f8a2d5b
SunshineAtNoon <8500575@qq.com>
2015-05-25 19:11:50 +0800

    finished supervised_dnn_cost

--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -18,6 +18,7 @@ gradStack = cell(numHidden+1, 1);
 %% forward prop
 %%% YOUR CODE HERE %%%
 %隐藏层
+
 for l = 1:numHidden
     if(l == 1)
         z = stack{l}.W * data;
@@ -31,6 +32,7 @@ end
 h  = exp(bsxfun(@plus,stack{numHidden+1}.W * hAct{numHidden},stack{numHidden+1}.b));
 pred_prob = bsxfun(@rdivide,h,sum(h,1));
 hAct{numHidden+1} = pred_prob;%最后一层输出的实际上是预测的分类结果
+  
 
 %% return here if only predictions desired.
 if po
@@ -39,15 +41,45 @@ if po
   return;
 end;
 
-%% compute cost
+%% compute cost计算softmax函数的损失函数
 %%% YOUR CODE HERE %%%
+logp = log2(pred_prob);
+index = sub2ind(size(logp),labels',1:size(pred_prob,2));
+ceCost = -sum(logp(index));
 
 %% compute gradients using backpropagation
+
 %%% YOUR CODE HERE %%%
+%输出层
+output = zeros(size(pred_prob));
+output(index) = 1;
+error = pred_prob - output;
+
+for l = numHidden+1 : -1 :1
+    gradStack{l}.b = sum(error,2);
+    if(l == 1)
+        gradStack{l}.W = error * data';
+        break;
+    else
+        gradStack{l}.W = error * hAct{l-1}';
+    end
+    error = (stack{l}.W)'*error .* hAct{l-1} .* (1-hAct{l-1});
+end
 
 %% compute weight penalty cost and gradient for non-bias terms
 %%% YOUR CODE HERE %%%
+%penalty cost
+wCost = 0;
+for l = 1:numHidden+1
+    wCost = wCost + 0.5*ei.lambda * sum(stack{l}.W(:) .^ 2);
+end
+cost = ceCost + wCost;
 
+%gradient for non-bias terms
+for l = numHidden:-1:1
+    gradStack{l}.W = gradStack{l}.W + ei.lambda * stack{l}.W;
+end
+ 
 %% reshape gradients into vector
 [grad] = stack2params(gradStack);
 end
